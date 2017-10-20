03b19e72392a0495395fe21f2e88d6c9dc1d80ae
CmouseG <1668924806@qq.com>
2017-10-20 11:24:49 -0700

    context function

--- client/client.js
+++ client/client.js
@@ -6,13 +6,13 @@ const opn = require('opn')
 const app = connect()
 
 app.use(serveStatic('./'))
-app.use('/api', proxy({
+app.use(proxy(function (pathname, req) {
+    return pathname.match('.*\.do')
+},
+{
     target: 'http://127.0.0.1:4000', // must protocol http://
     changeOrigin: true,
-    logLevel: 'debug',
-    pathRewrite: {
-        '^/api/path1': '/api/path2'
-    }
+    logLevel: 'debug'
 }))
 app.listen(3000)
 console.log('Server starting: http://127.0.0.1:3000')
