58ce8cd9ece0187d447a26bfa1b7d92aabb9dadc
CmouseG <1668924806@qq.com>
2017-10-20 09:05:06 -0700

    routes

--- api-connect/test/api-hammock.js
+++ api-connect/test/api-hammock.js
@@ -2,29 +2,37 @@ const tape = require('tape')
 const routesRouter = require('routes-router')
 const mockRequest = require('hammock/request')
 const mockResponse = require('hammock/response')
+const routes = require('../routes')
 
 tape('error hammock', function (assert) {
     const router = routesRouter()
 
-    router.addRoute("*", function (req, res, opts, cb) {
-        cb(new Error("some error"))
-    })
+    // router.addRoute("/api/error", function (req, res, opts, cb) {
+    //     cb(new Error("some error"))
+    // })
+    router.addRoute("/api/error", routes.error.index)
 
     router(
         mockRequest({
-            url: '/foo',
+            url: '/api/error',
             headers: { host: 'localhost', bar: 'baz' },
             method: 'GET'
         }),
         mockResponse(function (err, res) {
             assert.ifError(err)
 
-            assert.equal(res.statusCode, 500)
+            assert.equal(res.statusCode, 502)
             const body = JSON.parse(res.body)
 
             assert.deepEqual(body, {
                 errors: [
-                    { message: "some error", attribute: "general" }
+                    {
+                        type: 'error',
+                        statusCode: 502,
+                        message: '系统繁忙，请稍后再试！',
+                        _name: 'ErrorError',
+                        attribute: 'general'
+                    }
                 ]
             })
 
